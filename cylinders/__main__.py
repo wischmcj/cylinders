efd119a504a321b9273089450ee795d8738b244e
wischmcj <wischmcj@miamioh.edu>
2023-10-14 19:49:01 -0400

    Removing unused imps, combining files

--- cylinders/__main__.py
+++ cylinders/__main__.py
@@ -1,6 +1,6 @@
 """Entry point for cylinders."""
 
-from .cliOld import main  # pragma: no cover
+from .cliOrig import main  # pragma: no cover
 
 if __name__ == "__main__":  # pragma: no cover
     main()
